01486d8fecb689d976f4c9652096814ed624b344
Arth9vansola <arthvansola@gmail.com>
2025-07-06 20:36:59 +0530

    changes happen and new files are created

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+import { Key } from 'lucide-react';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers(){
+    return [
+      {
+         source:'/embed',
+         headers:[
+          {
+            key:'content-security-policy',
+            value:"frame-src 'self' https://motiongo-waitlist.created.app;"
+          }
+         ]
+        
+      }
+  ]
+  }
+};
 
 export default nextConfig;
